32fbfa8543dc9a017c92d86c3685900bfd612ef5
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2025-10-25 10:44:04 +0300

    Check if the vehicle carries a package.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,20 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
     string _packageTag = "Package";
     string _customerTag = "Customer";
+    bool _hasPackage;
+
+    [SerializeField]
+    float _destroyDelay = 0.5f;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(_packageTag))
+        if (collision.CompareTag(_packageTag) && _hasPackage == false)
         {
             Debug.Log("We picked it up!");
+            _hasPackage = true;
+            Destroy(collision.GameObject(), _destroyDelay);
+
         }
-        else if (collision.CompareTag(_customerTag))
+        else if (collision.CompareTag(_customerTag) && _hasPackage)
         {
             Debug.Log("The package has been delivered to the customer!");
+            _hasPackage = false;
         }
-        
     }
 }
