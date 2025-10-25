060914426f64691c40f2bddbc078992d2a289b07
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2025-10-25 11:32:53 +0300

    Play particles when the delivery is picked up.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,13 +9,18 @@ public class Delivery : MonoBehaviour
     bool _hasPackage;
 
     [SerializeField]
+    ParticleSystem _particle;
+
+    [SerializeField]
     float _destroyDelay = 0.5f;
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(_packageTag) && _hasPackage == false)
         {
             Debug.Log("We picked it up!");
+            _particle.Play();
             _hasPackage = true;
             Destroy(collision.GameObject(), _destroyDelay);
 
@@ -22,6 +28,7 @@ public class Delivery : MonoBehaviour
         else if (collision.CompareTag(_customerTag) && _hasPackage)
         {
             Debug.Log("The package has been delivered to the customer!");
+            _particle.Stop();
             _hasPackage = false;
         }
     }
